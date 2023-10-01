d0dcdcc0ebd7d709f8c68051f1aeea9a00abfc25
dacadla <71786326+dacadla@users.noreply.github.com>
2023-10-01 19:55:50 +0200

    Remove unnecessary files and complete the hardware quiz

--- pages/hardware-quiz.php
+++ pages/hardware-quiz.php
@@ -138,7 +138,7 @@ $quiz = [
     ],
 ];
 
-// Initialize variables to keep track of user's score and submitted status
+// Initialize variables
 $score = 0;
 $submitted = false;
 $perfectScoreMessage = "Congratulations, you should be proud!";
@@ -169,52 +169,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php
-    // Include the same CSS file used in your main site
-    echo '<link rel="stylesheet" href="../css/styles.css">';
-    ?>
+    <link rel="stylesheet" href="../css/styles.css">
     <title>Hardware Quiz</title>
 </head>
 <body>
-    <header>
-        <h1>Computer Hardware Quiz</h1>
-    </header>
+<header>
+    <h1>Computer Hardware Quiz</h1>
+</header>
 
-    <main>
-        <section>
-    			<?php if (!$submitted): ?>
-  		      <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-          	  <?php foreach ($quiz as $key => $question): ?>
-                <fieldset>
-                  <legend><?= 'Question ' . ($key + 1) . ': ' . $question['question'] ?></legend>
-                  <?php foreach ($question['options'] as $optionKey => $option): ?>
-                    <label>
-                    <input type="radio" name="q<?= ($key + 1) ?>" value="<?= $optionKey ?>">
-                    <?= $option ?>
-                    </label><br>
-                  <?php endforeach; ?>
-              	</fieldset>
-            	<?php endforeach; ?>
-            <button type="submit">Submit</button>
-        </form>
-    		<?php else: ?>
-        	<?php if ($allQuestionsAnswered): ?>
-            <p>Your score: <?= $score ?> out of <?= count($quiz) ?></p>
-            	<?php if ($score === count($quiz)): ?>
-                <p><?= $perfectScoreMessage ?></p>
-            	<?php endif; ?>
-        	<?php else: ?>
-            <p>Please answer all questions before submitting the quiz.</p>
-            <p><a href="hardware-quiz.php" class="button">Retry</a></p>
-        	<?php endif; ?>
-        <p><a href="hardware.php" class="button">Back to Hardware Overview</a></p>
-	    <?php endif; ?>
-</section>
+<main>
+    <section>
+        <?php if (!$submitted): ?>
+            <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
+                <?php foreach ($quiz as $key => $question): ?>
+                    <fieldset>
+                        <legend><?= 'Question ' . ($key + 1) . ': ' . $question['question'] ?></legend>
+                        <?php foreach ($question['options'] as $optionKey => $option): ?>
+                            <label>
+                                <input type="radio" name="q<?= ($key + 1) ?>" value="<?= $optionKey ?>">
+                                <?= $option ?>
+                            </label><br>
+                        <?php endforeach; ?>
+                    </fieldset>
+                <?php endforeach; ?>
+                <button type="submit">Submit</button>
+            </form>
+        <?php else: ?>
+            <?php if ($allQuestionsAnswered): ?>
+                <p>Your score: <?= $score ?> out of <?= count($quiz) ?></p>
+                <p><a href="hardware-quiz.php" class="button">Retry</a></p>
+                <?php if ($score === count($quiz)): ?>
+                    <p><?= $perfectScoreMessage ?></p>
+                    <a href="software.php" class="button">Continue to software</a>
+                <?php endif; ?>
+            <?php else: ?>
+                <p>Please answer all questions before submitting the quiz.</p>
+                <p><a href="hardware-quiz.php" class="button">Retry</a></p>
+            <?php endif; ?>
+            <p><a href="hardware.php" class="button">Back to Hardware Overview</a></p>
+        <?php endif; ?>
+    </section>
+</main>
 
-    </main>
-
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Radovan Mihalik</p>
-    </footer>
+<footer>
+    <p>&copy; <?= date("Y"); ?> Radovan Mihalik</p>
+</footer>
 </body>
 </html>
